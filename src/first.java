d19a153dc394b8b4a08885521d9b181eed0c18aa
Mercury164 <152604364+Mercury164@users.noreply.github.com>
2024-10-03 13:37:35 +0300

    lab1

---
+++ src/first.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        if (x < y) {
+            x = x / 2;
+            System.out.println(x);
+        }
+        else {
+            y = y / 2;
+            System.out.println(y);
+        }
+    }
+}
