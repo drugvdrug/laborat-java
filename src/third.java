67c36d6344201e3b22bbf11559abb57c7c305ffa
Dima <dmitriy030804@gmail.com>
2024-10-03 13:39:34 +0300

    lab1

--- src/third.java
+++ src/third.java
@@ -5,6 +5,7 @@ public class third {
     public static void main(String[] args) {
         System.out.println("Задание 3");
 
+
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble();
 
